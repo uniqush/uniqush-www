7523368288b8e2cd733290ada8d1f72aae318a7c
Nan Deng <monnand@gmail.com>
2012-07-28 18:12:53 -0400

    automatic generate download page

--- ext/init.rb
+++ ext/init.rb
@@ -11,3 +11,67 @@ config = Webgen::WebsiteAccess.website.config
 config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/uniqush*.deb'
 config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/uniqush*.rpm'
 config['sourcehandler.patterns']['Webgen::SourceHandler::Copy'] << '**/uniqush*.tar.gz'
+
+download_prefix = 
+    <<-eos
+---
+title: Downloads
+in_menu: true
+author: Nan Deng
+author_url: http://monnand.me
+---
+
+##Downloads
+
+eos
+
+downloadf = File.open("src/downloads.page", "w+")
+downloadf.write(download_prefix)
+
+programs = []
+versions = []
+archs = []
+package_types = ["rpm", "deb", "tar.gz"]
+
+archconv = {}
+package_types.each do | pkg |
+    archconv[pkg] = {}
+end
+
+archconv["rpm"]["amd64"] = "x86_64"
+archconv["tar.gz"]["amd64"] = "x86_64"
+archconv["deb"]["amd64"] = "amd64"
+
+prog_ver_sep = {}
+prog_ver_sep["rpm"] = "-"
+prog_ver_sep["tar.gz"] = "_"
+prog_ver_sep["deb"] = "_"
+
+ver_arch_sep = {}
+ver_arch_sep["rpm"] = "-1."
+ver_arch_sep["tar.gz"] = "_"
+ver_arch_sep["deb"] = "_"
+
+Dir.foreach("src/downloads") do |package|
+    if (package =~ /uniqush-.+\.deb/)
+        deb_pattern = /(.+)_(.+)_(.+)\.deb/
+        m = deb_pattern.match(package)
+        programs << m[1]
+        versions << m[2]
+        archs << m[3]
+    end
+end
+
+programs.each do | prog |
+    downloadf.write("###" + prog + "\n\n")
+    archs.each do | arch |
+        downloadf.write("####" + arch + "\n\n")
+        versions.each do | ver |
+            package_types.each do | pkg |
+                arch_name = archconv[pkg][arch]
+                filename = prog + prog_ver_sep[pkg] + ver + ver_arch_sep[pkg] + arch_name + "." + pkg
+                downloadf.write("- [" + pkg + "](downloads/" + filename + ")\n")
+            end
+        end
+    end
+end
